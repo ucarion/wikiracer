3c322e3e7dde55f36b83a10dc3c4f6898a4aec15
Ulysse Carion <ulysse@ulysse.io>
2017-08-01 23:17:06 -0700

    Add two-way search

--- wikipath/explorer.go
+++ wikipath/explorer.go
@@ -7,8 +7,8 @@ import (
 
     "cgt.name/pkg/go-mwclient"
     "cgt.name/pkg/go-mwclient/params"
-
     "github.com/deckarep/golang-set"
+    "github.com/antonholmquist/jason"
 )
 
 const NUM_SIMULTANEOUS_QUERIES int = 10
@@ -20,7 +20,7 @@ type Hop struct {
     toArticle string
 }
 
-func Explore(done <-chan struct{}, source string) <-chan Hop {
+func Explore(done <-chan struct{}, source string, forward bool) <-chan Hop {
     out := make(chan Hop)
     visited := mapset.NewSet()
     waitGroup := sync.WaitGroup{}
@@ -36,7 +36,7 @@ func Explore(done <-chan struct{}, source string) <-chan Hop {
 
         visited.Add(article)
         fmt.Printf("Exploring: %s\n", article)
-        for hop := range getLinks(article, throttle) {
+        for hop := range getLinks(article, forward, throttle) {
             select {
             case out <- hop:
             case <-done:
@@ -45,7 +45,11 @@ func Explore(done <-chan struct{}, source string) <-chan Hop {
             }
 
             waitGroup.Add(1)
-            go exploreArticle(hop.toArticle)
+            if forward {
+                go exploreArticle(hop.toArticle)
+            } else {
+                go exploreArticle(hop.fromArticle)
+            }
         }
     }
 
@@ -73,22 +77,11 @@ func init() {
 // Wikipedia gives these articles when following redirects
 var BLACKLIST mapset.Set = mapset.NewSetFromSlice([]interface{}{"H:L", "H:S"})
 
-func getLinks(title string, throttle <-chan time.Time) <-chan Hop {
+func getLinks(title string, forward bool, throttle <-chan time.Time) <-chan Hop {
     out := make(chan Hop)
 
     go func() {
-        queryValues := params.Values{
-            "titles": title,
-            "prop":   "links",
-            "pllimit": "500",
-
-            // Wikimedia has a concept of a "namespace", which distinguishes
-            // articles like "Apple", "User:Apple", "Category:Fruits", etc. For our
-            // purposes, only namespace 0, corresponding to articles, is relevant.
-            "plnamespace": "0",
-        }
-
-        query := wikiClient.NewQuery(queryValues)
+        query := wikiClient.NewQuery(queryParams(title, forward))
 
         throttledNext := func() bool {
             <-throttle
@@ -98,40 +91,20 @@ func getLinks(title string, throttle <-chan time.Time) <-chan Hop {
         for throttledNext() {
             response := query.Resp()
 
-            resultPages, err := response.GetObjectArray("query", "pages")
+            var resultKey string
+            if forward {
+                resultKey = "pages"
+            } else {
+                resultKey = "backlinks"
+            }
+
+            resultPages, err := response.GetObjectArray("query", resultKey)
             if err != nil {
                 panic(err)
             }
 
             for _, resultPage := range resultPages {
-                title, err := resultPage.GetString("title")
-                if err != nil {
-                    panic(err)
-                }
-
-                // Each "page" corresponds to one of the titles given to Wikimedia,
-                // and every response Wikimedia returns will contain a page for each
-                // title.
-                //
-                // Some of these pages will not have any links in them, because all
-                // the links are in other pages in this response.
-                if _, ok := resultPage.Map()["links"]; ok {
-                    links, err := resultPage.GetObjectArray("links")
-                    if err != nil {
-                        panic(err)
-                    }
-
-                    for _, linkObject := range links {
-                        linkTitle, err := linkObject.GetString("title")
-                        if err != nil {
-                            panic(err)
-                        }
-
-                        if !BLACKLIST.Contains(linkTitle) {
-                            out <- Hop{title, linkTitle}
-                        }
-                    }
-                }
+                processResultPage(out, forward, title, resultPage)
             }
         }
 
@@ -143,4 +116,63 @@ func getLinks(title string, throttle <-chan time.Time) <-chan Hop {
     }()
 
     return out
+}
+
+func processResultPage(out chan<- Hop, forward bool, queryTitle string, resultPage *jason.Object) {
+    title, err := resultPage.GetString("title")
+    if err != nil {
+        panic(err)
+    }
+
+    if !forward {
+        out <- Hop{title, queryTitle}
+        return
+    }
+
+    // Each "page" corresponds to one of the titles given to Wikimedia, and
+    // every response Wikimedia returns will contain a page for each title.
+    //
+    // Some of these pages will not have any links in them, because all the
+    // links are in other pages in this response.
+    if _, ok := resultPage.Map()["links"]; ok {
+        links, err := resultPage.GetObjectArray("links")
+        if err != nil {
+            panic(err)
+        }
+
+        for _, linkObject := range links {
+            linkTitle, err := linkObject.GetString("title")
+            if err != nil {
+                panic(err)
+            }
+
+            if !BLACKLIST.Contains(linkTitle) {
+                out <- Hop{title, linkTitle}
+            }
+        }
+    }
+}
+
+func queryParams(title string, forward bool) params.Values {
+    if forward {
+        return params.Values{
+            "titles": title,
+            "prop":   "links",
+            "pllimit": "500",
+
+            // Wikimedia has a concept of a "namespace", which distinguishes
+            // articles like "Apple", "User:Apple", "Category:Fruits", etc. For our
+            // purposes, only namespace 0, corresponding to articles, is relevant.
+            "plnamespace": "0",
+        }
+    } else {
+        return params.Values {
+            "list": "backlinks",
+            "bltitle": title,
+            "bllimit": "500",
+
+            // See comment above for explanation.
+            "blnamespace": "0",
+        }
+    }
 }
--- wikipath/wikipath.go
+++ wikipath/wikipath.go
@@ -1,35 +1,100 @@
 package wikipath
 
-import "fmt"
+import (
+    "fmt"
+)
 
 func Search(source, target string) []string {
     done := make(chan struct{})
     defer close(done)
 
+    // reverse links from articles toward articles "closer" to source
     towardSource := make(map[string]string)
+    // forward links from articles toward articles "closer" to target
+    towardTarget := make(map[string]string)
 
     fmt.Println("Starting search ...");
+    fromSource := Explore(done, source, true)
+    fromTarget := Explore(done, target, false)
 
-    for hop := range(Explore(done, source)) {
-        // only insert if not already present
-        if _, ok := towardSource[hop.toArticle]; !ok {
-            towardSource[hop.toArticle] = hop.fromArticle
-        }
+    for pair := range merge(fromSource, fromTarget) {
+        isFromSource := pair.bool
+        hop := pair.Hop
 
-        if hop.toArticle == target {
-            return solutionPath(towardSource, source, target)
+        if isFromSource {
+            // if not yet known, add info about path back to source
+            if _, ok := towardSource[hop.toArticle]; !ok {
+                towardSource[hop.toArticle] = hop.fromArticle
+            }
+
+            // see if a path from toArticle to target is known
+            if _, ok := towardTarget[hop.toArticle]; ok {
+                return solutionPath(towardSource, towardTarget, source, target, hop.toArticle)
+            }
+        } else {
+            // if not yet known, add info about path toward target
+            if _, ok := towardTarget[hop.fromArticle]; !ok {
+                towardTarget[hop.fromArticle] = hop.toArticle
+            }
+
+            // see if a path from fromArticle to source is known
+            if _, ok := towardSource[hop.fromArticle]; ok {
+                return solutionPath(towardSource, towardTarget, source, target, hop.fromArticle)
+            }
         }
     }
 
     return nil
 }
 
+// merge both streams, and close the output as once either input closes
+func merge(fromSource, fromTarget <-chan Hop) <-chan struct{Hop; bool} {
+    out := make(chan struct{Hop; bool})
+    stopped := false
 
-func solutionPath(towardSource map[string]string, source string, target string) []string {
-    if source == target {
-        return []string{target}
+    output := func(c <-chan Hop, isFromSource bool) {
+        for hop := range c {
+            if !stopped {
+                out <- struct{Hop; bool}{hop, isFromSource}
+            }
+        }
+
+        fmt.Println("One source stopped...")
+        stopped = true
+        close(out)
+    }
+
+    go output(fromSource, true)
+    go output(fromTarget, false)
+
+    return out
+}
+
+func solutionPath(
+    towardSource, towardTarget map[string]string,
+    source, target, middle string,
+) []string {
+    sourceToMiddle := halfSolutionPath(towardSource, source, middle)
+    middleToTarget := halfSolutionPath(towardTarget, target, middle)
+    reverse(middleToTarget)
+
+    return append(append(sourceToMiddle, middle), middleToTarget...)
+}
+
+// returns a path through `links` from `start` to `objective`, excluding `start`.
+func halfSolutionPath(links map[string]string, objective, start string) []string {
+    if start == objective {
+        return []string{}
     } else {
-        previous := towardSource[target]
-        return append(solutionPath(towardSource, source, previous), target)
+        next := links[start]
+        return append(halfSolutionPath(links, objective, next), next)
+    }
+}
+
+func reverse(a []string) {
+    // https://github.com/golang/go/wiki/SliceTricks#reversing
+    for i := len(a) / 2 - 1; i >= 0; i-- {
+        opp := len(a) - 1 - i
+        a[i], a[opp] = a[opp], a[i]
     }
 }
